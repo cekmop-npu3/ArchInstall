ae3363b18670bcd2c5e505c0390b9782ea59a21f
myname <test.dqw4@gmail.com>
2026-02-21 23:47:35 +0300

    Fixed indentation

--- symlinks.sh
+++ symlinks.sh
@@ -34,56 +34,56 @@ function resolveOpts () {
     eval set -- "$newOpts"
     
     if [[ "$1" == "--" && "$#" == 1 ]]; then
-	usage
+	    usage
     fi
 
     while [[ $1 != "--" ]]; do
-	case $1 in 
-	    (-h|--help)
-		usage
-	    ;;
-	    (-u|--username)
-		if ! id "${2:-}" &>/dev/null; then
-		    echo "User \"$2\" does not exist"
-		    exit $INVALID_USERNAME
-		fi
-		username="$2"
-	    ;;
-	    (-p|--configPath)
-		if ! [[ -e "$2" ]]; then
-		    echo "Configuration file \"$2\" is not found"
-		    exit $INVALID_CONFIG_PATH
-		fi
-		configPath="$2"
-	    ;;
-	    (-a|--action)
-		if [[ ${2:="create"} != "create" && "$2" != "delete" ]]; then
-		    echo "Action parameter must be either create or delete, not \"$2\""
-		    exit $INVALID_ACTION
-		fi
-		action="$2"
-	    ;;
-	esac
-	shift 2
+	    case $1 in 
+	        (-h|--help)
+		        usage
+	        ;;
+	        (-u|--username)
+		        if ! id "${2:-}" &>/dev/null; then
+		            echo "User \"$2\" does not exist"
+		            exit $INVALID_USERNAME
+		        fi
+		        username="$2"
+	        ;;
+	        (-p|--configPath)
+		        if ! [[ -e "$2" ]]; then
+		            echo "Configuration file \"$2\" is not found"
+		            exit $INVALID_CONFIG_PATH
+		        fi
+		        configPath="$2"
+	        ;;
+	        (-a|--action)
+		        if [[ ${2:="create"} != "create" && "$2" != "delete" ]]; then
+		            echo "Action parameter must be either create or delete, not \"$2\""
+		            exit $INVALID_ACTION
+		        fi
+		        action="$2"
+	        ;;
+	    esac
+	    shift 2
     done
 
     shift 1
     if [[ -n ${1:-} ]]; then
-	echo "Unknown param \"$1\" specified"
-	exit $PARAM_SPECIFIED
+	    echo "Unknown param \"$1\" specified"
+	    exit $PARAM_SPECIFIED
     fi
 }
 
 function checkVariables () {
     if [[ -z ${username:-} ]]; then 
-	echo "Username was not specified"
-	exit $INVALID_USERNAME
+	    echo "Username was not specified"
+	    exit $INVALID_USERNAME
     elif [[ -z ${configPath:-} ]]; then 
-	echo "Config path was not specified"
-	exit $INVALID_CONFIG_PATH
+	    echo "Config path was not specified"
+	    exit $INVALID_CONFIG_PATH
     elif [[ -z ${action:-} ]]; then 
-	echo "Action was not specified"
-	exit $INVALID_ACTION
+	    echo "Action was not specified"
+	    exit $INVALID_ACTION
     fi
 }
 
@@ -91,9 +91,9 @@ function expandConfigFile () {
     local subs=( $(grep -oP '\$\K\w+(?=\s*|$|/)' "$configPath") )
     local sub=
     for sub in ${subs[@]}; do
-	local -n ref="$sub"
-	echo "$ref"
-	sed -i "s/\$$sub/$ref/" $configPath
+	    local -n ref="$sub"
+	    echo "$ref"
+	    sed -i "s/\$$sub/$ref/" $configPath
     done
 }
 
@@ -111,31 +111,30 @@ function parseConfig () {
     local -i lineCount=0
 
     for args in "${targets[@]}"; do
-	target=$(echo "$args" | grep -oP '^\S+(?=\s+)')
-	link=$(echo "$args" | grep -oP '\s+\K.+$')
+	    target=$(echo "$args" | grep -oP '^\S+(?=\s+)')
+	    link=$(echo "$args" | grep -oP '\s+\K.+$')
 
-	if ! [[ -e "$target" ]]; then
-	    echo "Target is invalid"
-	    exit 1
-	fi
+	    if ! [[ -e "$target" ]]; then
+	        echo "Target is invalid"
+	        exit 1
+	    fi
 
-	case $action in
-	    (create)
-		if ! ln -sf "$target" "$link"; then
-		    echo "Invalid args at line: $lineCount"
-		    exit $INVALID_CONFIG_ARGS
-		fi
-	    ;;
-	    (delete)
-		if ! unlink "$link"; then
-		    echo "Invalid symlink at line: $lineCount"
-		    exit $INVALID_SYMLINK
-		fi
-	    ;;
-	esac
-	(( ++lineCount ))
+	    case $action in
+	        (create)
+		        if ! ln -sf "$target" "$link"; then
+		            echo "Invalid args at line: $lineCount"
+		            exit $INVALID_CONFIG_ARGS
+		        fi
+	        ;;
+	        (delete)
+		        if ! unlink "$link"; then
+		            echo "Invalid symlink at line: $lineCount"
+		            exit $INVALID_SYMLINK
+		        fi
+	        ;;
+	    esac
+	    (( ++lineCount ))
     done
-
 }
 
 parseConfig $@
